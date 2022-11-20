9f6551ff4ec9cbc2fb4c73aab42a76646c201637
lizardkinglk <chansanfdo@gmail.com>
2022-11-20 07:09:28 +0000

    Update SignUp.styles.ts and SignUp.tsx

--- components/SignUp/SignUp.tsx
+++ components/SignUp/SignUp.tsx
@@ -2,21 +2,21 @@ import { Title, Text, Anchor } from '@mantine/core';
 import useStyles from './SignUp.styles';
 
 export function SignUp() {
-    const { classes } = useStyles();
+  const { classes } = useStyles();
 
-    return (
-        <>
-            <Title className={classes.title} align="center" mt={100}>
-                <Text inherit variant="gradient" component="span">
-                    Sign Up
-                </Text>
-            </Title>
-            <Text color="dimmed" align="center" size="lg" sx={{ maxWidth: 580 }} mx="auto" mt="xl">
-                {/* This is the preview of the site.{' '}
+  return (
+    <>
+      <Title className={classes.title} align="center" mt={100}>
+        <Text inherit variant="text" component="span">
+          Sign Up
+        </Text>
+      </Title>
+      <Text color="dimmed" align="center" size="lg" sx={{ maxWidth: 580 }} mx="auto" mt="xl">
+        {/* This is the preview of the site.{' '}
                 <Anchor href="/signup" size="lg">
                     Sign Up
                 </Anchor>{' '}here to start the awesomeness */}
-            </Text>
-        </>
-    );
+      </Text>
+    </>
+  );
 }
